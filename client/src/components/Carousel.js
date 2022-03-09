1b39951bb5c02424628be812f4f1530991256b23
Matthew Lawrence <Mattplawrence@gmail.com>
2022-03-09 09:48:29 -0500

    changed thumbnails

--- client/src/components/Carousel.js
+++ client/src/components/Carousel.js
@@ -6,7 +6,12 @@ import two from "./assets/images/forest_bkgd.jpg";
 import three from "./assets/images/freegan_logo.gif";
 import four from "./assets/images/love_neighbor_wall.jpg";
 import five from "./assets/images/sunset.jpg";
-import thumb from "./assets/images/thumb-19.jpg";
+import thumb1 from "./assets/images/thumb-19.jpg";
+import thumb2 from "./assets/images/thumb-20.jpg";
+import thumb3 from "./assets/images/thumb-21.jpg";
+import thumb4 from "./assets/images/thumb-22.jpg";
+import thumb5 from "./assets/images/thumb-25.jpg";
+import thumb6 from "./assets/images/thumb-24.jpg";
 
 class Carousel extends Component {
   componentDidMount() {
@@ -31,7 +36,7 @@ class Carousel extends Component {
       >
         <div className="carousel-item red white-text" href="#one!">
           <h5>Item</h5>
-          <img src="http://placecorgi.com/260/180?no_track=1" />
+          <img src={thumb1} />
           <p className="white-text">
             Bacon ipsum dolor amet tempor fugiat hamburger in, alcatra
             adipisicing salami nostrud fatback reprehenderit ground round short
@@ -42,7 +47,7 @@ class Carousel extends Component {
         </div>
         <div className="carousel-item amber white-text" href="#two!">
           <h5>Item</h5>
-          <img src="http://placecorgi.com/260/180?no_track=4" />
+          <img src={thumb2} />
           <p className="white-text">
             Veniam reprehenderit jerky sunt pork chop tenderloin biltong, in sed
             magna picanha. Capicola occaecat in, ipsum corned beef jerky irure
@@ -51,7 +56,7 @@ class Carousel extends Component {
         </div>
         <div className="carousel-item green white-text" href="#three!">
           <h5>Item</h5>
-          <img src="http://placecorgi.com/260/180?no_track=2" />
+          <img src={thumb3} />
           <p className="white-text">
             Bacon ipsum dolor amet tempor fugiat hamburger in, alcatra
             adipisicing salami nostrud fatback reprehenderit ground round short
@@ -62,7 +67,7 @@ class Carousel extends Component {
         </div>
         <div className="carousel-item blue white-text" href="#four!">
           <h5>Item</h5>
-          <img src="http://placecorgi.com/260/180?no_track=3" />
+          <img src={thumb4} />
           <p className="white-text">
             Veniam reprehenderit jerky sunt pork chop tenderloin biltong, in sed
             magna picanha. Capicola occaecat in, ipsum corned beef jerky irure
@@ -71,7 +76,7 @@ class Carousel extends Component {
         </div>
         <div className="carousel-item red white-text" href="#four!">
           <h5>Item</h5>
-          <img src="http://placecorgi.com/260/180?no_track=4" />
+          <img src={thumb5} />
           <p className="white-text">
             Bacon ipsum dolor amet tempor fugiat hamburger in, alcatra
             adipisicing salami nostrud fatback reprehenderit ground round short
@@ -82,7 +87,7 @@ class Carousel extends Component {
         </div>
         <div className="carousel-item blue white-text" href="#four!">
           <h5>Item</h5>
-          <img src="http://placecorgi.com/260/180?no_track=2" />
+          <img src={thumb6} />
           <p className="white-text">
             Veniam reprehenderit jerky sunt pork chop tenderloin biltong, in sed
             magna picanha. Capicola occaecat in, ipsum corned beef jerky irure
